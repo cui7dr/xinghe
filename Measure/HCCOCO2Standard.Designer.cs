aa4ccc29ef8589db9b487803a3b3c8d69dc70e15
cui7dr <cui7dr@gmail.com>
2019-05-24 11:42:00 +0800

    修改 HC,CO,CO2 标准气浓度界面

--- Measure/HCCOCO2Standard.Designer.cs
+++ Measure/HCCOCO2Standard.Designer.cs
@@ -50,9 +50,15 @@
             this.sure = new System.Windows.Forms.Button();
             this.cali = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.HC_text = new System.Windows.Forms.TextBox();
+            this.CO_text = new System.Windows.Forms.TextBox();
+            this.CO2_text = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // index
@@ -295,38 +301,104 @@
             this.label1.TabIndex = 21;
             this.label1.Text = "请输入标准气的浓度值，每通道输入完成后按“确定”键，\r\n全部输入完成后按“校准”键，并通入标准气…";
             // 
-            // textBox1
+            // HC_text
             // 
-            this.textBox1.Font = new System.Drawing.Font("宋体", 32.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox1.Location = new System.Drawing.Point(268, 320);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(159, 57);
-            this.textBox1.TabIndex = 22;
+            this.HC_text.Font = new System.Drawing.Font("宋体", 32.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.HC_text.Location = new System.Drawing.Point(268, 43);
+            this.HC_text.Name = "HC_text";
+            this.HC_text.Size = new System.Drawing.Size(159, 57);
+            this.HC_text.TabIndex = 22;
             // 
-            // textBox2
+            // CO_text
             // 
-            this.textBox2.Font = new System.Drawing.Font("宋体", 32.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox2.Location = new System.Drawing.Point(268, 187);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(159, 57);
-            this.textBox2.TabIndex = 23;
+            this.CO_text.Font = new System.Drawing.Font("宋体", 32.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.CO_text.Location = new System.Drawing.Point(268, 178);
+            this.CO_text.Name = "CO_text";
+            this.CO_text.Size = new System.Drawing.Size(159, 57);
+            this.CO_text.TabIndex = 23;
             // 
-            // textBox3
+            // CO2_text
             // 
-            this.textBox3.Font = new System.Drawing.Font("宋体", 32.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox3.Location = new System.Drawing.Point(268, 38);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(159, 57);
-            this.textBox3.TabIndex = 24;
+            this.CO2_text.Font = new System.Drawing.Font("宋体", 32.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.CO2_text.Location = new System.Drawing.Point(268, 314);
+            this.CO2_text.Name = "CO2_text";
+            this.CO2_text.Size = new System.Drawing.Size(159, 57);
+            this.CO2_text.TabIndex = 24;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 27.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(433, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 37);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "10";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(483, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 16);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "-6";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 27.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(433, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 37);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "10";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 27.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(433, 334);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 37);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "10";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(483, 198);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 16);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "-2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.Location = new System.Drawing.Point(483, 334);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 16);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "-2";
             // 
             // HCCOCO2Standard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1280, 720);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CO2_text);
+            this.Controls.Add(this.CO_text);
+            this.Controls.Add(this.HC_text);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cali);
             this.Controls.Add(this.sure);
@@ -377,11 +449,17 @@
         private System.Windows.Forms.Button num8;
         private System.Windows.Forms.Button num9;
         private System.Windows.Forms.Button num0;
-        private System.Windows.Forms.Button sure;
-        private System.Windows.Forms.Button cali;
+        private System.Windows.Forms.Button sure; //确定
+        private System.Windows.Forms.Button cali; //校准
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox HC_text;
+        private System.Windows.Forms.TextBox CO_text;
+        private System.Windows.Forms.TextBox CO2_text;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
